861d427905690e364d4c72ffa93efc0488d7781d
Ruchi Bhalchandra Bhogate <rbhogate@silo.luddy.indiana.edu>
2024-01-12 19:26:39 -0500

    milestone 1 attempt 2

--- kernel/system/resched.c
+++ kernel/system/resched.c
@@ -1,11 +1,12 @@
 #include <barelib.h>
-#include <syscall.h>
+// #include <syscall.h>
 
 /*  'resched' places the current running thread into the ready state  *
  *  and  places it onto  the tail of the  ready queue.  Then it gets  *
  *  the head  of the ready  queue  and sets this  new thread  as the  *
  *  'current_thread'.  Finally,  'resched' uses 'ctxsw' to swap from  *
  *  the old thread to the new thread.                                 */
-int32 resched(void) {
+int32 resched(void)
+{
   return 0;
 }
